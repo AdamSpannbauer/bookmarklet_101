e5f07277e831bfd94344f4deccd61a1f6affbed4
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-08-01 15:09:22 -0400

    print word count

--- replace/yeet_replace.js
+++ replace/yeet_replace.js
@@ -3,6 +3,7 @@
   for (var i = 0; i < paragraphs.length; i++) {
   	// count words in p tag
   	pWordCount = paragraphs[i].innerHTML.split(' ').length;
+  	console.log('word count' + pWordCount);
   	// replace each word with YEET
   	if (pWordCount > 0) {
   		paragraphs[i].innerHTML = 'YEET '.repeat(pWordCount);
