ac1413c848d1efa20094ffb450d83fe6a13ad4ea
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-08-01 15:39:40 -0400

    print p innerhtml

--- replace/yeet_replace.js
+++ replace/yeet_replace.js
@@ -1,9 +1,10 @@
 (function() {
   var paragraphs = document.getElementsByTagName('p');
   for (var i = 0; i < paragraphs.length; i++) {
+  	console.log(paragraphs[i].innerHTML);
   	// count words in p tag
-  	pWordCount = paragraphs[i].innerHTML.split(' ').length;
-  	console.log('word count' + pWordCount);
+  	var pWordCount = paragraphs[i].innerHTML.split(' ').length;
+  	console.log('word count ' + pWordCount);
   	// replace each word with YEET
   	if (pWordCount > 0) {
   		paragraphs[i].innerHTML = 'YEET '.repeat(pWordCount);
