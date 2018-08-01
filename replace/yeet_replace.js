c6aab81edc5bacf9036fecc45eb72bf5962604b8
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-08-01 15:43:16 -0400

    stop print p innerhtml

--- replace/yeet_replace.js
+++ replace/yeet_replace.js
@@ -1,7 +1,7 @@
 (function() {
   var paragraphs = document.getElementsByTagName('p');
   for (var i = 0; i < paragraphs.length; i++) {
-  	console.log(paragraphs[i].innerHTML);
+  	// console.log(paragraphs[i].innerHTML);
   	// count words in p tag
   	var pWordCount = paragraphs[i].innerHTML.split(' ').length;
   	console.log('word count ' + pWordCount);
